ab33633c36fff981889e4f5b4cfcd71ceff47a30
Alex Antonyuk <alex@antonyuk.me>
2013-09-03 15:40:56 +0300

    added job's info command

--- jenkins.rb
+++ jenkins.rb
@@ -28,15 +28,91 @@ end
 
 J = JenkinsApi::Client.new params
 
+## helpers
+
+def parameter_info parameter
+  defaultValue = parameter[:defaultParameterValue][:value]
+  defaultValuePrint = " (default = #{defaultValue})" unless defaultValue.empty?
+  
+  type = parameter[:type] if parameter.key? :type
+  typePrint = "[#{type.sub('ParameterDefinition', '')}]" if type
+  
+  choices_list = parameter[:choices] if parameter.key? :choices
+  choices = []
+  if choices_list
+    choices_list.each do |c|
+      choices << c
+    end
+  end
+  {
+    :name => parameter[:name],
+    :description => parameter[:description],
+    :defaultValue => defaultValue,
+    :defaultValuePrint => defaultValuePrint,
+    :type => type,
+    :typePrint => typePrint,
+    :choices => choices
+  }
+end
+
 ## Body
 
 command :list do |c|
-  c.syntax = 'jenkins list [filter]'
-  c.description = 'List all jobs'
+  c.syntax = "jenkins list [filter]"
+  c.description = "List all jobs"
   c.action do |args, options|
-    filter = '.*'
+    filter = ".*"
     filter = ".*#{args[0]}.*" if args.count > 0
     puts "Jobs:"
     puts "\t" + J.job.list(filter).join("\n\t")
+  end
+end
+
+command :info do |c|
+  c.syntax = "jenkins info <job_name>"
+  c.description = "Display job's description"
+  c.action do |args, options|
+    data = Hashr.new(J.job.list_details(args[0])) unless args.empty?
+    if data
+      puts "\nJob: #{data.displayName}"
+      puts "Description: #{data.description}"
+      puts "Last build: ##{data.lastBuild.number}"
+      puts ""
+      params_data = data.property.delete_if {|x| !x[:parameterDefinitions]}.first
+      params_data = params_data[:parameterDefinitions] if params_data.key? :parameterDefinitions
+      params = []
+      if params_data
+        puts "Parameters:"
+        params_data.each do |p|
+          info = parameter_info p
+          params << info
+          puts "* #{info[:name]} #{info[:defaultValuePrint]} - #{info[:description]} #{info[:typePrint]}"
+          unless info[:choices].empty?
+            puts "Choices:"
+            info[:choices].each do |c|
+              puts "\t#{c}"
+            end
+          end
+          puts ""
+        end
+      end
+      
+      print %Q{Build: jenkins build "#{data.name}" }
+      params.each do |param|
+        defaultValue = "param value"
+        defaultValue = param[:defaultValue] unless param[:defaultValue].empty?
+        print %Q{#{param[:name]}="#{defaultValue}" }
+      end
+      puts ""
+    end
+  end
+end
+
+command :build do |c|
+  c.syntax = "jenkins build <job_name> [params]"
+  c.description = "Build job with parameters"
+  c.action do |args, options|
+    puts args
+    puts options
   end
 end
